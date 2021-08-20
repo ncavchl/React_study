fce8909d4de903950b8f5a0d4dc87715ca69252c
ncavchl <ncavchl>
2021-08-20 16:40:41 +0900

    검색

--- React-basic/contact-app/src/Components/PhoneInfo.js
+++ React-basic/contact-app/src/Components/PhoneInfo.js
@@ -7,6 +7,13 @@ class PhoneInfo extends Component {
     phone: "",
   };
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state !== nextState) {
+      return true;
+    }
+    return this.props.info !== nextProps.info;
+  }
+
   handleRemove = () => {
     const { info, onRemove } = this.props;
     console.log(info.id);
@@ -47,6 +54,7 @@ class PhoneInfo extends Component {
       margin: "8px",
     };
 
+    console.log(name);
     return (
       <div style={style}>
         {editing ? (
