5bd1658217b87f394addcff37fcab2c6f832cf90
ncavchl <ncavchl>
2021-08-20 16:55:05 +0900

    last

--- React-basic/contact-app/src/Components/PhoneForm.js
+++ React-basic/contact-app/src/Components/PhoneForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 class PhoneForm extends Component {
+  input = React.createRef();
+
   state = {
     name: "",
     phone: "",
@@ -19,6 +21,7 @@ class PhoneForm extends Component {
       name: "",
       phone: "",
     });
+    this.input.current.focus();
   };
 
   render() {
@@ -29,6 +32,7 @@ class PhoneForm extends Component {
           placeholder="이름"
           onChange={this.handleChange}
           value={this.state.name}
+          ref={this.input}
         />
         <input
           name="phone"
