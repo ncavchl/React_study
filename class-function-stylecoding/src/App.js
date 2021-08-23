980690b532ad99be7e2e0b11000e23a023cc6bca
ncavchl <ncavchl>
2021-08-23 13:43:01 +0900

    useState hook

--- class-function-stylecoding/src/App.js
+++ class-function-stylecoding/src/App.js
@@ -1,29 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <h1>Hello world</h1>
-      <FuncComp></FuncComp>
-      <ClassComp></ClassComp>
+      <FuncComp initnumber={2}></FuncComp>
+      <ClassComp initnumber={2}></ClassComp>
     </div>
   );
 }
 
-function FuncComp() {
+function FuncComp(props) {
+  const [number, setNumber] = useState(234);
+  const [date, setDate] = useState(new Date().toString());
   return (
     <div className="container">
       <h2>function style component</h2>
+      <p>Number : {number}</p>
+      <p>Date : {date}</p>
+      <input
+        type="button"
+        value="random"
+        onClick={function () {
+          setNumber(Math.random());
+        }}
+      ></input>
+      <input
+        type="button"
+        value="date"
+        onClick={function () {
+          setDate(new Date().toString());
+        }}
+      ></input>
     </div>
   );
 }
 
 class ClassComp extends React.Component {
+  state = {
+    number: this.props.initnumber,
+    date: new Date().toString(),
+  };
   render() {
     return (
       <div className="container">
         <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
+        <input
+          type="button"
+          value="random"
+          onClick={function () {
+            this.setState({ number: Math.random() });
+          }.bind(this)}
+        ></input>
+        <input
+          type="button"
+          value="date"
+          onClick={function () {
+            this.setState({ date: new Date().toString() });
+          }.bind(this)}
+        ></input>
       </div>
     );
   }
